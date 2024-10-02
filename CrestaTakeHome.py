92fcf5d6b5bd42029ffde3ffd66d0938821aa708
Joseph Laux <jlaux@Laux-MBP.local>
2024-10-02 14:28:25 -0400

    Editting main script file

---
+++ CrestaTakeHome.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def create_df(table_name, database_uri):
+    engine = create_engine(database_uri)
+    query = f"SELECT agent_id, call_duration_sec FROM {table_name}"
+    df = pd.read_sql(query, engine)
+    return df
+
+def find_avg_call_length(df):
+    average_call_length = df.groupby("agent_id")["call_duration_sec"].mean()
+    return agent_avg_call_length
+
+def find_percentile_call_length(df, percentile):
+    agent_call_length = df.groupby("agent_id")["call_duration_sec"].quantile(percentile)
+    return agent_call_length
+
+
+if __name__ == "__main__":
+    database_uri = "sqlite:///conversations.db"
+    table_name = "conversations"
+    conversation_df = create_df(table_name, database_uri)
+    average_df = find_avg_call_length(conversation_df)
+    percentile_df = find_percentile_call_length(conversation_df, 90)
+    customer_info_df = pd.merge(average_df, percentile_df, on='id')
+    customer_info_df.to_csv("customer_info.csv", index=False)
+
+    print(customer_info_df)
